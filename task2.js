8a513479b5ed46d299c5740b05427a6c6b86ca3f
Dabelu Otubelu <dabelu2004@gmail.com>
2023-05-19 14:57:09 +0100

    Task 1 and 2 done

--- task2.js
+++ task2.js
@@ -1,11 +1,15 @@
+//Function to convert Kelvin to Celsius
 function KelvinToCelsius(Kelvin) {
     return Kelvin -273.15;
 }
 
+//Function to convert Kelvin to Celsius
 function KelvinToFahrenheit(Kelvin) {
     return (Kelvin -273.15) * 9/5 + 32;
 }
 
+//Function to convert Kelvin to Celsius
 function KelvinToNewton(Kelvin) {
     return (Kelvin -273.15) * 0.33;
 }
+ 
